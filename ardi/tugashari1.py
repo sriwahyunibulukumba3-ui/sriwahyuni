c2668a0dd0a39420fd95550f01f4823c7276be8f
Sri Wahyuni <sriwahyunibulukumba3@gmail.com>
2026-04-06 21:25:03 +0700

    pria solo

---
+++ ardi/tugashari1.py
@@ -0,0 +1,4 @@
+print("programmer hebat lahir tanpa error")
+print("bagi saya, tulisan merah di layar bukanlah kegagalan")
+print ("melainkan petunjuk unyuk belajar lebih banyak")
+print("mari mulai ngoding")
