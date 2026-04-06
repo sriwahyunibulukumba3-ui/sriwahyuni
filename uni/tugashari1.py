cda69bd8b6fe0b131ad65a09d379e40e2cb09f8b
Sri Wahyuni <sriwahyunibulukumba3@gmail.com>
2026-04-06 21:38:24 +0700

    pria solo

---
+++ uni/tugashari1.py
@@ -0,0 +1,5 @@
+print("Selamat sore!")
+print("Terima kasih kepada Kakak Pembimbing atas arahannya hari ini.")
+print("Saya bangga bisa melaksankan PKL di Diskominfo & Persandian Bulukumba.")
+print("Mohon bimbingannya untuk 4 bulan kedepan.")
+ 
