facb29bc2960c50012cf2c9177abecbfd0fbc370
Sri Wahyuni <sriwahyunibulukumba3@gmail.com>
2026-04-15 13:00:23 +0700

    uni

--- ardi/teks21.py
+++ ardi/teks21.py
@@ -2,8 +2,50 @@
 # merubah dari satu tipe ke tipe lain
 # tipe data = int, float, str, bool
 
+## INTEGER
+print("====INTEGER====")
 data_int = 9;
 print("data = ", data_int, ",type =",type(data_int))
 
 data_float = float(data_int)
-print("data =", data_float, ",type =",type(data_float))
+data_str = str(data_int)
+data_bool = bool(data_int) # akan false jika nilai int = 0
+print("data = ", data_float, ", type =",type(data_float))
+print("data = ", data_str, ", type =",type(data_float))
+print("data = ", data_bool, ", type =",type(data_float))
+
+## FLOAT
+print("====FLOAT====")
+data_float = 9.9;
+print("data = ", data_float, ",type =",type(data_float))
+
+data_float = float(data_float) # akan di bulatkan kebawah
+data_str = str(data_float)
+data_bool = bool(data_float) # akan false jika nilai float = 0
+print("data = ", int, ", type =",type(data_float))
+print("data = ", data_str, ", type =",type(data_str))
+print("data = ", data_bool, ", type =",type(data_bool))
+
+## BOOLEAN
+print("====BOOLEAN====")
+data_bool = False
+print("data = ", data_bool, ",type =",type(data_bool))
+data_float = float(data_bool) # akan di bulatkan kebawah
+data_str = str(data_bool)
+data_float = float(data_bool) # akan false jika nilai float = 0
+print("data = ", int, ", type =",type(data_float))
+print("data = ", data_str, ", type =",type(data_str))
+print("data = ", data_float, ", type =",type(data_bool))
+
+## STRING
+print("====STRING====")
+data_str = "10";
+print("data = ", data_str, ", type =",type(data_str)),
+
+data_int    = int(data_str) # string harus angka
+data_float  = float(data_str) # string harus angka
+data_bool   = bool(data_str) # false jika string kosong
+print("data = ", data_int, ",type =",type(data_int))
+print("data = ", data_float, ",type =",type(data_float))
+print("data = ", data_bool, ",type =",type(data_bool))
+
